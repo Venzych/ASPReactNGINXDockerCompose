6bd792363cc030a7c016802670f8189f881679bf
Venzy <kosilow130603@mail.ru>
2024-12-09 13:26:39 +0300

    Добавьте файлы проекта.

---
+++ ASPReactNGINXDockerCompose/Controllers/UsersController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPReactNGINXDockerCompose.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetUser()
+        {
+            var users = new[]
+            {
+                new { name = "Oleg"},
+                new { name = "Kirill"}
+            };
+            return Ok(users);
+        }
+    }
+}
